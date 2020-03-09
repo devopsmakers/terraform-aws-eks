8f04b8dd8b092daac74debf44e9a81456a1b1c4b
Tim Birkett <tim.birkett@sainsburys.co.uk>
2020-03-09 17:44:06 +0000

    Make node groups locals a little more readable

--- modules/node_groups/locals.tf
+++ modules/node_groups/locals.tf
@@ -1,15 +1,17 @@
 locals {
+  node_group_defaults = {
+    iam_role_arn     = concat(aws_iam_role.node_groups.*.arn, [""])[0]
+    instance_type    = "m4.large"  # Size of the node group instances.
+    desired_capacity = "1"         # Desired node capacity in the autoscaling group and changing its value will not affect the autoscaling group's desired capacity because the cluster-autoscaler manages up and down scaling of the nodes. Cluster-autoscaler add nodes when pods are in pending state and remove the nodes when they are not required by modifying the desirec_capacity of the autoscaling group. Although an issue exists in which if the value of the asg_min_size is changed it modifies the value of asg_desired_capacity.
+    max_capacity     = "3"         # Maximum node group capacity in the autoscaling group.
+    min_capacity     = "1"         # Minimum node group capacity in the autoscaling group. NOTE: Change in this paramater will affect the asg_desired_capacity, like changing its value to 2 will change asg_desired_capacity value to 2 but bringing back it to 1 will not affect the asg_desired_capacity.
+    key_name         = ""          # The key name that should be used for the instances in the autoscaling group
+    subnets          = var.subnets # A list of subnets to place the nodes in. i.e. ["subnet-123", "subnet-456", "subnet-789"]
+  }
+
   # Merge defaults and per-group values to make code cleaner
   node_groups_expanded = { for k, v in var.node_groups : k => merge(
-    {
-      iam_role_arn     = aws_iam_role.node_groups[0].arn
-      instance_type    = "m4.large"  # Size of the node group instances.
-      desired_capacity = "1"         # Desired node capacity in the autoscaling group and changing its value will not affect the autoscaling group's desired capacity because the cluster-autoscaler manages up and down scaling of the nodes. Cluster-autoscaler add nodes when pods are in pending state and remove the nodes when they are not required by modifying the desirec_capacity of the autoscaling group. Although an issue exists in which if the value of the asg_min_size is changed it modifies the value of asg_desired_capacity.
-      max_capacity     = "3"         # Maximum node group capacity in the autoscaling group.
-      min_capacity     = "1"         # Minimum node group capacity in the autoscaling group. NOTE: Change in this paramater will affect the asg_desired_capacity, like changing its value to 2 will change asg_desired_capacity value to 2 but bringing back it to 1 will not affect the asg_desired_capacity.
-      key_name         = ""          # The key name that should be used for the instances in the autoscaling group
-      subnets          = var.subnets # A list of subnets to place the nodes in. i.e. ["subnet-123", "subnet-456", "subnet-789"]
-    },
+    local.node_group_defaults,
     var.node_groups_defaults,
     v,
   ) if var.create_eks }
