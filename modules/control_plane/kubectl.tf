2838035cf992dc2fc044f686e02b15d4dd59cc8b
Tim Birkett <tim.birkett@sainsburys.co.uk>
2020-03-09 11:26:29 +0000

    fix(kubeconfig): Set sensible file and directory permissions on kubeconfig file

--- modules/control_plane/kubectl.tf
+++ modules/control_plane/kubectl.tf
@@ -1,6 +1,8 @@
 
 resource "local_file" "kubeconfig" {
-  count    = var.write_kubeconfig && var.create_eks ? 1 : 0
-  content  = data.template_file.kubeconfig[0].rendered
-  filename = substr(var.config_output_path, -1, 1) == "/" ? "${var.config_output_path}kubeconfig_${var.cluster_name}" : var.config_output_path
+  count                = var.write_kubeconfig && var.create_eks ? 1 : 0
+  content              = data.template_file.kubeconfig[0].rendered
+  filename             = substr(var.config_output_path, -1, 1) == "/" ? "${var.config_output_path}kubeconfig_${var.cluster_name}" : var.config_output_path
+  directory_permission = "0750"
+  file_permission      = "0600"
 }
