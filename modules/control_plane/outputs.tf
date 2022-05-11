51b3bfbbfb24ed8f4cdeb5ecc841c294224aaabb
Rom Gaigi <romg@pecan.ai>
2022-05-11 08:20:14 +0200

    change list to tolist in the needed modules

--- modules/control_plane/outputs.tf
+++ modules/control_plane/outputs.tf
@@ -1,26 +1,26 @@
 output "cluster_id" {
   description = "The name/id of the EKS cluster."
-  value       = element(concat(aws_eks_cluster.this.*.id, tolist("")), 0)
+  value       = element(concat(aws_eks_cluster.this.*.id, [""]), 0)
 }
 
 output "cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = element(concat(aws_eks_cluster.this.*.arn, tolist("")), 0)
+  value       = element(concat(aws_eks_cluster.this.*.arn, [""]), 0)
 }
 
 output "cluster_certificate_authority_data" {
   description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
-  value       = element(concat(aws_eks_cluster.this[*].certificate_authority[0].data, tolist("")), 0)
+  value       = element(concat(aws_eks_cluster.this[*].certificate_authority[0].data, [""]), 0)
 }
 
 output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
-  value       = element(concat(aws_eks_cluster.this.*.endpoint, tolist("")), 0)
+  value       = element(concat(aws_eks_cluster.this.*.endpoint, [""]), 0)
 }
 
 output "cluster_version" {
   description = "The Kubernetes server version for the EKS cluster."
-  value       = element(concat(aws_eks_cluster.this[*].version, tolist("")), 0)
+  value       = element(concat(aws_eks_cluster.this[*].version, [""]), 0)
 }
 
 output "cluster_security_group_id" {
